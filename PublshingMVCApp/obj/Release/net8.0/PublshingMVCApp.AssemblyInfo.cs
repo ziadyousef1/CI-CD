c9c248578dbb588402777f51260bff5913f35e01
Ziad Yousef <zy40768@gmail.com>
2024-11-26 14:18:07 +0200

    Testing Publishing to Azure

--- PublshingMVCApp/obj/Release/net8.0/PublshingMVCApp.AssemblyInfo.cs
+++ PublshingMVCApp/obj/Release/net8.0/PublshingMVCApp.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PublshingMVCApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ed66036ffae02b66237d2aff8fa0a5b2b1955ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("PublshingMVCApp")]
